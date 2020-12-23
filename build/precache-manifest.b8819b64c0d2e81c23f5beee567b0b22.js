ede95d6c019c26b94a2a1221b2beed099465c504
Talai Sergei <tsa@tut.by>
2020-12-23 22:32:01 +0300

    add folder build

---
+++ build/precache-manifest.b8819b64c0d2e81c23f5beee567b0b22.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f7ed85fd831b0bfdaa3b568a13f731b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "114971d36892e08e1394",
+    "url": "/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "92db7c80bc28d8b61ed2",
+    "url": "/static/css/main.2f11d748.chunk.css"
+  },
+  {
+    "revision": "114971d36892e08e1394",
+    "url": "/static/js/2.07d07e37.chunk.js"
+  },
+  {
+    "revision": "f967dc5027287ef5399df5e36cc779ea",
+    "url": "/static/js/2.07d07e37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92db7c80bc28d8b61ed2",
+    "url": "/static/js/main.5f1101a8.chunk.js"
+  },
+  {
+    "revision": "e2c6e531b8324ac17d6c",
+    "url": "/static/js/runtime-main.206ac5b9.js"
+  },
+  {
+    "revision": "428c3f3666f166504ad5de58f5384433",
+    "url": "/static/media/rs-logo.428c3f36.svg"
+  },
+  {
+    "revision": "b18c11c6d542505f3e0544cd46dd5a05",
+    "url": "/static/media/sprite.b18c11c6.svg"
+  }
+]);
